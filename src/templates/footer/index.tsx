ad44750d3f50c5baaa0c081eb0e315f8d9202ae9
pavel <pavelpp1974@gmail.com>
2024-12-01 21:18:07 +0300

    fixfooter link

--- src/templates/footer/index.tsx
+++ src/templates/footer/index.tsx
@@ -3,6 +3,7 @@ import Vk from '../../assets/vk.svg?react';
 import T from '../../assets/t.svg?react';
 import Inst from '../../assets/inst.svg?react';
 import styles from './index.module.css';
+import { Link } from 'react-router-dom';
 
 export const Footer = () => {
   return (
@@ -12,23 +13,27 @@ export const Footer = () => {
         <span className={styles.menu_title}>Кемпы и тренировки</span>
         <ul className={styles.menu}>
           <li>
-            <a href="/weekendCamps">Кемпы на выходные</a>
+            <Link to="/weekendCamps">Кемпы на выходные</Link>
           </li>
           <li>
-            <a href="/longCamps">Кемпы длинные</a>
+            <Link to="/longCamps">Кемпы длинные</Link>
           </li>
           <li>
-            <a href="/trainingSchedule">Расписание тренировок</a>
+            <Link to="/trainingSchedule">Расписание тренировок</Link>
           </li>
         </ul>
       </div>
       <div>
         <span className={styles.menu_title}>О школе</span>
         <ul className={styles.menu}>
-          <li>О нас</li>
+          <li>
+            <Link to="/about">О нас</Link>
+          </li>
           <li>Отзывы</li>
           <li>Часто задаваемые вопросы</li>
-          <li>Тренеры</li>
+          <li>
+            <Link to="/allCoahes">Тренеры</Link>
+          </li>
         </ul>
       </div>
       <div>
