bbefa38f94da8205ef3aeaf9a6eaf837417dd4fc
pavel <pavelpp1974@gmail.com>
2024-12-15 08:20:10 +0300

    fix title

--- src/root.tsx
+++ src/root.tsx
@@ -16,7 +16,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>My App</title>
+        <title>Magic Volley</title>
         <Meta />
         <Links />
       </head>
