5f7da1a1541f8bf8033337ba5939108b53477626
pavel <pavelpp1974@gmail.com>
2024-12-20 22:43:45 +0300

    return roles

--- src/auth/interface.ts
+++ src/auth/interface.ts
@@ -3,6 +3,6 @@ export type IUserRole = 'ADMIN' | 'USER' | 'MODERATOR' | 'GUEST';
 export interface IUser {
   email: string;
   id: string;
-  role: IUserRole;
+  roles: IUserRole[];
   username: string;
 }
