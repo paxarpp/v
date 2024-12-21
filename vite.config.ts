81551709ec8f5fac7de111019c0ef0b5b9057c76
pavel <pavelpp1974@gmail.com>
2024-12-21 17:18:04 +0300

    add env file update build entry point

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { reactRouter } from '@react-router/dev/vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRouter(), svgr()],
-  server: {
-    proxy: {
-      '/magicvolley': 'http://localhost:8081',
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const url = loadEnv(mode, process.cwd(), '').URL_ENTRY_POINT;
+  return {
+    plugins: [reactRouter(), svgr()],
+    server: {
+      proxy: {
+        '/magicvolley': 'http://localhost:8081',
+      },
+      port: 3000,
     },
-    port: 3000,
-  },
+    base: mode !== 'production' ? '/' : url,
+  };
 });
