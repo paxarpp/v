b1dbc7cf42dc53b743a54e01fe925cc0f066dead
pavel <pavelpp1974@gmail.com>
2024-12-22 16:34:14 +0300

    add env for api and port

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,24 @@ import { reactRouter } from '@react-router/dev/vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export default defineConfig(({ mode }) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  const url = loadEnv(mode, process.cwd(), '').URL_ENTRY_POINT;
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const API_URL = `${env.VITE_API_URL ?? 'http://localhost:8081'}`;
+  const PORT = `${env.VITE_PORT ?? '3000'}`;
+  const url = `${env.URL_ENTRY_POINT ?? '/'}`;
+
   return {
     plugins: [reactRouter(), svgr()],
     server: {
       proxy: {
-        '/magicvolley': mode !== 'production' ? 'http://localhost:8081' : '',
+        '/magicvolley': API_URL,
       },
-      port: 3000,
+      port: PORT,
     },
     base: mode !== 'production' ? '/' : url,
   };
