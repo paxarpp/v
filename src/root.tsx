56d44ec011adfaa4aa711cd6acb0f7358ff036b0
pavel <pavelpp1974@gmail.com>
2025-01-30 21:36:08 +0300

    font fix

--- src/root.tsx
+++ src/root.tsx
@@ -22,6 +22,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+          rel="stylesheet"
+        />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Magic Volley</title>
