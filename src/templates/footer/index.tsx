e5b1676b6c764ebc21a9fb88a55e72633a2fddc6
pavel <pavelpp1974@gmail.com>
2025-04-09 21:12:23 +0300

    fix footer

--- src/templates/footer/index.tsx
+++ src/templates/footer/index.tsx
@@ -17,68 +17,70 @@ export const Footer: React.FC<IProps> = ({ linkTg, linkInstagram, linkVk }) => {
   const { isMobile } = useDeviceDetect();
   return (
     <footer className={isMobile ? styles.footer_mobi : styles.footer}>
-      <div>
-        <Link to="/">
-          <Logo />
-        </Link>
-        <br />
-        <span className={`${styles.message} ${styles.mt_50}`}>
-          ИП Кочетков Михаил Олегович
-        </span>
-        <span className={styles.message}>ИНН 623412482291</span>
-        <span className={styles.message}>ОГРНИП 325620000000329</span>
-      </div>
-      <div>
-        <span className={styles.menu_title}>Пляжный волейбол</span>
-        <ul className={styles.menu}>
-          <li>
-            <Link to="/weekendCamps">Кемпы выходного дня</Link>
-          </li>
-          <li>
-            <Link to="/longCamps">Недельные кемпы</Link>
-          </li>
-          <li>
-            <Link to="/beachCoaches">Тренеры</Link>
-          </li>
-          <li>
-            <Link to="/oldCamps">Прошедшие кемпы</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <span className={styles.menu_title}>Классический волейбол</span>
-        <ul className={styles.menu}>
-          <li>
-            <Link to="/trainingSchedule">Расписание</Link>
-          </li>
-          <li>
-            <Link to="/classicCoaches">Тренеры</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <Link to="/childCamps" className={styles.menu_title}>
-          Детский лагерь
-        </Link>
-      </div>
-      <div>
-        <span className={styles.menu_title}>О нас</span>
-        <ul className={styles.menu}>
-          <li>
-            <Link to="/about#reviews">Отзывы</Link>
-          </li>
-          <li>
-            <Link to="/#questions">Часто задаваемые вопросы</Link>
-          </li>
-          <li>
-            <Link to="/agreement">Политика обработки персональных данных</Link>
-          </li>
-          <li>
-            <Link to="/agreement">Публичная оферта</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
+      <div className={styles.footer_col}>
+        <div>
+          <Link to="/">
+            <Logo />
+          </Link>
+          <br />
+          <span className={`${styles.message} ${styles.mt_50}`}>
+            ИП Кочетков Михаил Олегович
+          </span>
+          <span className={styles.message}>ИНН 623412482291</span>
+          <span className={styles.message}>ОГРНИП 325620000000329</span>
+        </div>
+        <div>
+          <span className={styles.menu_title}>Пляжный волейбол</span>
+          <ul className={styles.menu}>
+            <li>
+              <Link to="/weekendCamps">Кемпы выходного дня</Link>
+            </li>
+            <li>
+              <Link to="/longCamps">Недельные кемпы</Link>
+            </li>
+            <li>
+              <Link to="/beachCoaches">Тренеры</Link>
+            </li>
+            <li>
+              <Link to="/oldCamps">Прошедшие кемпы</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <span className={styles.menu_title}>Классический волейбол</span>
+          <ul className={styles.menu}>
+            <li>
+              <Link to="/trainingSchedule">Расписание</Link>
+            </li>
+            <li>
+              <Link to="/classicCoaches">Тренеры</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <Link to="/childCamps" className={styles.menu_title}>
+            Детский лагерь
+          </Link>
+        </div>
+        <div>
+          <span className={styles.menu_title}>О нас</span>
+          <ul className={styles.menu}>
+            <li>
+              <Link to="/about#reviews">Отзывы</Link>
+            </li>
+            <li>
+              <Link to="/#questions">Часто задаваемые вопросы</Link>
+            </li>
+            <li>
+              <Link to="/agreement">Политика обработки персональных данных</Link>
+            </li>
+            <li>
+              <Link to="/agreement">Публичная оферта</Link>
+            </li>
+          </ul>
+        </div>
+        </div>
+        <div>
         <a href={linkVk} target={'_blank'} className={styles.icon}>
           <Vk />
         </a>
