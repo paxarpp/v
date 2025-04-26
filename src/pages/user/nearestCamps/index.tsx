19381bcf1110ec677ec00441ee3ab079b39ace30
pavel <pavelpp1974@gmail.com>
2025-04-26 12:46:20 +0300

    fix

--- src/pages/user/nearestCamps/index.tsx
+++ src/pages/user/nearestCamps/index.tsx
@@ -1,11 +1,22 @@
 import { useLoaderData } from 'react-router';
 import { Route } from '../+types';
 import { Scroller } from '../scroller';
+import { useDeviceDetect } from '../../../hooks';
+import { CampsMobileScroller } from '../../../templates/CampMobileScroller';
+import styles from '../index.module.css';
 
 export const NearestCamps = () => {
   const { user } = useLoaderData<Route.ComponentProps['loaderData']>();
+  const { isMobile } = useDeviceDetect();
 
-  return user?.isAdmin ? null : (
+  return user?.isAdmin ? null : isMobile ? (
+    <div className={styles.camps_wrap_mobi}>
+      <h2 className={styles.camps_header_mobi}>{'Мои ближайшие кемпы'}</h2>
+      <div className={styles.camps_scroller_mobi}>
+        <CampsMobileScroller list={user?.nearestCamps || []} />
+      </div>
+    </div>
+  ) : (
     <Scroller title={'Мои ближайшие кемпы'} camps={user?.nearestCamps} />
   );
 };
--- src/pages/user/pastCamps/index.tsx
+++ src/pages/user/pastCamps/index.tsx
@@ -1,11 +1,22 @@
 import { useLoaderData } from 'react-router';
 import { Route } from '../+types';
 import { Scroller } from '../scroller';
+import { useDeviceDetect } from '../../../hooks';
+import { CampsMobileScroller } from '../../../templates/CampMobileScroller';
+import styles from '../index.module.css';
 
 export const PastCamps = () => {
   const { user } = useLoaderData<Route.ComponentProps['loaderData']>();
+  const { isMobile } = useDeviceDetect();
 
-  return user?.isAdmin ? null : (
+  return user?.isAdmin ? null : isMobile ? (
+    <div className={styles.camps_wrap_mobi}>
+      <h2 className={styles.camps_header_mobi}>{'Мои прошедшие кемпы'}</h2>
+      <div className={styles.camps_scroller_mobi}>
+        <CampsMobileScroller list={user?.pastCamps || []} />
+      </div>
+    </div>
+  ) : (
     <Scroller title={'Мои прошедшие кемпы'} camps={user?.pastCamps} />
   );
 };
