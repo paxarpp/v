8c5f25089c16108172f773598beb793433ef9729
pavel <pavelpp1974@gmail.com>
2025-04-26 20:22:06 +0300

    fix

--- src/pages/coaches/classic.tsx
+++ src/pages/coaches/classic.tsx
@@ -2,8 +2,9 @@ import { CoachesList } from './coachesList';
 import { pl } from '../../api/pageLoader';
 import { ICoach, IImage } from './interfaces';
 import { api } from '../../api/api';
-import styles from './index.module.css';
 import { ImagesList } from './images';
+import { useDeviceDetect } from '../../hooks';
+import styles from './index.module.css';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export async function clientLoader() {
@@ -17,10 +18,15 @@ export async function clientLoader() {
 }
 
 export default function Coaches() {
+  const { isMobile } = useDeviceDetect();
   return (
     <div>
-      <h2 className={styles.title_top}>Тренерский состав</h2>
-      <h2 className={styles.title_down}>в школе волейбола Magic Volley</h2>
+      <h2 className={isMobile ? styles.title_top_mobi : styles.title_top}>
+        Тренерский состав
+      </h2>
+      <h2 className={isMobile ? styles.title_down_mobi : styles.title_down}>
+        в школе волейбола Magic Volley
+      </h2>
       <CoachesList />
 
       <ImagesList />
