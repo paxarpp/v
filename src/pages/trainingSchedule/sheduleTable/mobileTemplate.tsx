38b3042fad09707dd1d2cebf40532227fd2a538e
pavel <pavelpp1974@gmail.com>
2025-05-04 12:30:05 +0300

    fix

--- src/pages/trainingSchedule/sheduleTable/mobileTemplate.tsx
+++ src/pages/trainingSchedule/sheduleTable/mobileTemplate.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useLoaderData } from 'react-router';
 import { Route } from '../+types';
 import TGLink from '../../../assets/tgLink.svg?react';
@@ -8,6 +9,15 @@ import styles from '../index.module.css';
 export const MobileTemplate = () => {
   const { trainingShedule } =
     useLoaderData<Route.ComponentProps['loaderData']>();
+  const [showGroup, setShowGroup] = useState('');
+
+  const toggleGroup = (groupId: string) => {
+    if (showGroup === groupId) {
+      setShowGroup('');
+    } else {
+      setShowGroup(groupId);
+    }
+  };
 
   return (
     <div className={styles.container_groups_mobi}>
@@ -28,14 +38,28 @@ export const MobileTemplate = () => {
         });
 
         return (
-          <div key={group.id} className={styles.group_mobi}>
+          <div
+            key={group.id}
+            className={styles.group_mobi}
+            onClick={() => toggleGroup(group.id as string)}
+          >
             <div className={styles.group_name_mobi}>{group.name}</div>
-            <div className={styles.group_content_mobi}>
+            <div
+              className={
+                showGroup === group.id
+                  ? styles.show_group_content_mobi
+                  : styles.group_content_mobi
+              }
+            >
               <span>{timesByDay}</span>
               <br />
               <span>{group.days[0]?.address}</span>
 
-              <a href={createLinkTg(group.link)} target={'_blank'}>
+              <a
+                href={createLinkTg(group.link)}
+                target={'_blank'}
+                onClick={(e) => e.stopPropagation()}
+              >
                 <TGLink className={styles.tg_link} />
               </a>
             </div>
